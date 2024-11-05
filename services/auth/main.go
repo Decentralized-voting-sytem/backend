0a00d0868d480bfc8f268ab5a8abc1670d6bf008
samyak2003-coder <samyaksinghal2003@gmail.com>
2024-11-05 11:27:37 +0530

    added db folder

--- services/auth/main.go
+++ services/auth/main.go
@@ -1,25 +1,23 @@
 package main
 
 import (
-    "github.com/Decentralized-voting-sytem/backend/db/database"
     "github.com/Decentralized-voting-sytem/backend/services/auth/controllers"
+    "github.com/Decentralized-voting-sytem/backend/services/auth/db"
     "github.com/gin-contrib/cors"
     "github.com/gin-gonic/gin"
-    "gorm.io/gorm"
 )
 
-var DB *gorm.DB
 
 func main() {
-    DB = database.Init()
     r := gin.Default()
 
     config := cors.DefaultConfig()
     config.AllowOrigins = []string{"http://localhost:3000"}
     config.AllowHeaders = []string{"Content-Type"}
     config.AllowCredentials = true
-    r.Use(cors.New(config))
 
+    r.Use(cors.New(config))
+    db.InitDB()
     r.POST("/login", controllers.Login)
 
     r.Run()
